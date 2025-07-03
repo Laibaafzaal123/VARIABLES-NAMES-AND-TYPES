943efc405bfe56e525d963ee4f1240b3caa117cc
Laibaafzaal123 <laibafzaal779@gmail.com>
2025-07-03 16:18:22 +0500

    first commit

---
+++ varailesnamesillegalandlegal.js
@@ -0,0 +1,40 @@
+//Q1: Declare 3 variables in one statement. 
+var firstName, age, isStudent;
+
+//Q2:Declare 5 legal & 5 illegal variable names.
+// Legal Variable Names:
+var myName;
+var $price;
+var _total;
+var user1;
+var camelCase;
+//Illegal Variable Names:
+// var 1name;       // starts with number 
+// var my-name;     // hyphen is not allowed 
+// var var;         // reserved keyword 
+// var alert;       // reserved function name 
+// var user name;   // space is not allowed 
+
+//Q3:Display this in your browser
+// a) A heading stating:
+// “Rules for naming JS variables”
+
+// b) Fill in the blanks:
+// Variable names can only contain letters, numbers, underscores (_) and dollar signs ($).
+// For example: $my_1stVariable
+
+// c) Variables must begin with a:
+// letter, underscore (_) or dollar sign ($)
+// For example: $name, _name, or name
+
+// d) Variable names are case:
+// sensitive
+
+// e) Variable names should not be JS:
+// keywords
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letters, numbers, underscores (_) and dollar signs ($). For example: $my_1stVariable<br>");
+document.write("Variables must begin with a letter, underscore (_) or dollar sign ($). For example: $name, _name or name<br>");
+document.write("Variable names are case sensitive.<br>");
+document.write("Variable names should not be JS keywords.");
+
